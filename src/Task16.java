d355d9b90da7923560bb607e8ad88c92d2acd264
izuevro <56828102+izuevro@users.noreply.github.com>
2020-04-12 21:24:59 +0300

    Initial commit

---
+++ src/Task16.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Task16 {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("/Users/user/Desktop/test.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
