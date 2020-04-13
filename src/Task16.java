025cf5a2cb1f26c9b35934bc71200010d1f7da81
izuevro <56828102+izuevro@users.noreply.github.com>
2020-04-13 22:34:06 +0300

    Initial commit

--- src/Task16.java
+++ src/Task16.java
@@ -3,7 +3,7 @@ import java.io.FileReader;
 
 public class Task16 {
     public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new FileReader("/Users/user/Desktop/test.txt"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("/Users/romanzuev/Desktop/test.txt"))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
--- src/Task17.java
+++ src/Task17.java
@@ -7,11 +7,12 @@ public class Task17 {
     public static void main(String[] args) {
         System.out.println("Вводите значения в консоль. Для остановки введите слово stop");
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("/Users/user/Desktop/test.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("/Users/romanzuev/Desktop/test.txt"));
             String line;
             while (!(line = reader.readLine()).equals("stop")) {
                 writer.write(line);
             }
+            writer.close();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
